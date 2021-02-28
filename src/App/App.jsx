736bf5d48dba4085b419643b9c1a045f91fe9365
Shane O'Moore <shaneiadt@gmail.com>
2021-02-28 11:41:10 +0000

    re-organise folders

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './rocket.svg';
+import logo from '../assets/rocket.svg';
 import './App.css';
 
 function App() {
@@ -13,25 +13,7 @@ function App() {
   // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
     </div>
   );
 }
