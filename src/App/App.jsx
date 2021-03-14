e4e70af54a89ffbe465ba76a6a8c5fac99630c63
Shane O'Moore <shaneiadt@gmail.com>
2021-03-14 15:00:00 +0000

    load more data function

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -16,19 +16,7 @@ export default () => {
     'https://api.nasa.gov/planetary/apod?api_key=m5YApP4hrckiYPOPQxyYGiNfsLjtBn0k3g52bViv&count=10';
 
   useEffect(async () => {
-    try {
-      const response = await fetch(api);
-      const data = await response.json();
-
-      if (localStorage.getItem('nasa-apod-favorites')) {
-        setFavorites(JSON.parse(localStorage.getItem('nasa-apod-favorites')));
-      }
-
-      setData(data);
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-    }
+    await loadData();
   }, []);
 
   const isFavorite = (item) => favorites.find((i) => i.title === item.title);
@@ -45,6 +33,25 @@ export default () => {
       setMessage(null);
       clearTimeout(timeout);
     }, 1000);
+  };
+
+  const loadData = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch(api);
+      const data = await response.json();
+
+      if (localStorage.getItem('nasa-apod-favorites')) {
+        setFavorites(JSON.parse(localStorage.getItem('nasa-apod-favorites')));
+      }
+
+      setView('FEED');
+      setData(data);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const removeFromFavorites = (item) => {
@@ -68,7 +75,7 @@ export default () => {
         <Loader />
       ) : (
         <>
-          <Nav view={view} setView={setView} />
+          <Nav view={view} setView={setView} loadMore={loadData} />
           <Images
             images={view === 'FEED' ? data : favorites}
             addToFavorites={addToFavorites}
--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Nav.css';
 
-export default ({ view, setView }) => {
+export default ({ view, setView, loadMore }) => {
   return (
     <div className="container">
       <div className="navigation-container">
@@ -15,11 +15,9 @@ export default ({ view, setView }) => {
           >
             {view === 'FEED' ? 'Favorites' : 'Back to Feed'}
           </h3>
-          <h3>&nbsp;&nbsp;&nbsp;•&nbsp;&nbsp;&nbsp;</h3>
-          <h3 className="clickable">Load More</h3>
         </span>
         <span className="navigation-items hidden" id="favoritesNav">
-          <h3 className="clickable">Load More Nasa Images</h3>
+          <h3 className="clickable" onClick={() => loadMore()}>Load More Nasa Images</h3>
         </span>
       </div>
     </div>
